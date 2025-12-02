2fcda488f2b147fccd3f14193a18c62a3aead51d
AldiyarTopProgrammer <142615389+AldiyarTopProgrammer@users.noreply.github.com>
2025-12-02 20:58:53 +0500

    Update aqi_service.py

--- aqi_service.py
+++ aqi_service.py
@@ -1,8 +1,10 @@
 import requests
 import os
+from datetime import datetime, timedelta
 
-API_KEY = os.getenv("f36faaec08a2cdf58fe85cc986510752f8e1b45d")  # ты через Railway ENV задашь
+API_KEY = os.getenv("f36faaec08a2cdf58fe85cc986510752f8e1b45d")
 
+# ---------- Получение текущего AQI ----------
 def get_aqi(city: str):
     url = f"https://api.waqi.info/feed/{city}/?token={API_KEY}"
 
@@ -15,5 +17,39 @@ def get_aqi(city: str):
 
         return data["data"]["aqi"]
 
-    except Exception:
+    except:
         return None
+
+
+# ---------- История AQI за последние 3 дня ----------
+def get_history(city: str):
+    today = datetime.now()
+    history = {}
+
+    for i in range(3):
+        day = today - timedelta(days=i)
+        day_str = day.strftime("%Y-%m-%d")
+
+        url = f"https://api.waqi.info/feed/{city}/?token={API_KEY}"
+        r = requests.get(url).json()
+
+        aqi = r["data"]["aqi"] if r.get("status") == "ok" else "?"
+
+        history[day_str] = aqi
+
+    return history
+
+
+# ---------- Советы ----------
+def get_advice(aqi: int):
+    if aqi <= 50:
+        return "Все ок. Можно спокойно гулять."
+    if aqi <= 100:
+        return "Немного загрязнён. Но всё ещё норм."
+    if aqi <= 150:
+        return "Лучше не бегать на улице. Уменьши нагрузки."
+    if aqi <= 200:
+        return "Плохой воздух. Надень маску, по минимуму снаружи."
+    if aqi <= 300:
+        return "Очень вредно. Не выходи без нужды, закрывай окна."
+    return "Жесть. Сидеть дома, очиститель воздуха обязателен."
