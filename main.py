77d62807c7c91026e12d0f13a97462ada26a4f16
AldiyarTopProgrammer <142615389+AldiyarTopProgrammer@users.noreply.github.com>
2025-12-02 21:00:32 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import asyncio
+import os
 from aiogram import Bot, Dispatcher
-from config import BOT_TOKEN
+from aiogram.enums import ParseMode
+from aiogram.types import BotCommand
 from handlers import router
 
+TOKEN = os.getenv("8520976464:AAEXRuPMQjznxFX2vAmbI1hrFBwB8Scr0aY")
+
 async def main():
-    bot = Bot(BOT_TOKEN)
+    bot = Bot(token=TOKEN, parse_mode=ParseMode.HTML)
     dp = Dispatcher()
+
     dp.include_router(router)
 
-    print("Bot started")
+    await bot.set_my_commands([
+        BotCommand(command="start", description="Запустить бота"),
+    ])
+
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
