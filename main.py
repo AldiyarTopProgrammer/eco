a3bc1b06d18c0518b1195109209b841fdd9e1491
AldiyarTopProgrammer <142615389+AldiyarTopProgrammer@users.noreply.github.com>
2025-12-02 21:13:48 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-from aiogram import Bot, Dispatcher, executor
-from handlers import register_handlers
 import os
+import asyncio
+from aiogram import Bot, Dispatcher
+from handlers import register_handlers
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-bot = Bot(token=BOT_TOKEN)
-dp = Dispatcher(bot)
-
-register_handlers(dp)
+async def main():
+    bot = Bot(token=BOT_TOKEN)
+    dp = Dispatcher()
+    
+    register_handlers(dp)
+    
+    await dp.start_polling(bot)
 
 if __name__ == "__main__":
-    executor.start_polling(dp, skip_updates=True)
+    asyncio.run(main())
