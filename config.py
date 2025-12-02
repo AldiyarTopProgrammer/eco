4fab2eff94d716056f51aa681d8c1268376f0b94
AldiyarTopProgrammer <142615389+AldiyarTopProgrammer@users.noreply.github.com>
2025-12-02 20:58:02 +0500

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-BOT_TOKEN = os.getenv("8520976464:AAEXRuPMQjznxFX2vAmbI1hrFBwB8Scr0aY")
-AQI_TOKEN = os.getenv("f36faaec08a2cdf58fe85cc986510752f8e1b45d")  # Токен AQICN
-DEFAULT_CITY = "Astana"
